2351de7bc194516b8b1e07fcf0400b8eb30335a0
uukelele <robustrobot11@gmail.com>
2025-08-03 17:16:49 +0100

    make block place delay 0 by default

--- settings.js
+++ settings.js
@@ -45,7 +45,7 @@ const settings = {
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
     "chat_bot_messages": true, // publicly chat messages to other bots
 
-    "block_place_delay": 10, // delay between placing blocks (ms) if using newAction. helps avoid bot being kicked by anti-cheat mechanisms on servers.
+    "block_place_delay": 0, // delay between placing blocks (ms) if using newAction. helps avoid bot being kicked by anti-cheat mechanisms on servers.
   
     "log_all_prompts": false, // log ALL prompts to file
 }
