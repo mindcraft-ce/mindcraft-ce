1588a8770bd7290d2a9ea9cc6b997239f5ea85bf
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2025-08-23 16:00:59 -0500

    clean up azure

--- src/models/azure.js
+++ src/models/azure.js
@@ -1,22 +1,31 @@
 import { AzureOpenAI } from "openai";
-import { getKey } from '../utils/keys.js';
+import { getKey, hasKey } from '../utils/keys.js';
 import { GPT } from './gpt.js'
 
 export class AzureGPT extends GPT {
-    constructor(model_name, url, api_version, params) {
+    static prefix = 'azure';
+    constructor(model_name, url, params) {
         super(model_name, url)
 
         this.model_name = model_name;
-        this.params = params;
+        this.params = params || {};
 
-        let config = {}
+        const config = {};
 
         if (url)
             config.endpoint = url;
 
-        config.apiKey = getKey('OPENAI_API_KEY');
-        config.deployment = model_name; // This must be what you named the deployment in Azure, not the model version itself
-        config.apiVersion = api_version; // This is required for Azure
+        config.apiKey = hasKey('AZURE_OPENAI_API_KEY') ? getKey('AZURE_OPENAI_API_KEY') : getKey('OPENAI_API_KEY');
+
+        config.deployment = model_name;
+
+        if (this.params.apiVersion) {
+            config.apiVersion = this.params.apiVersion;
+            delete this.params.apiVersion; // remove from params for later use in requests
+        }
+        else {
+            throw new Error('apiVersion is required in params for azure!');
+        }
 
         this.openai = new AzureOpenAI(config)
     }
